dbb3be990c4b8ed2c19051930983fb18630664b3
FryMondo <ptachyksergei@gmail.com>
2023-03-06 00:29:04 +0200

    main

---
+++ main.js
@@ -0,0 +1,13 @@
+import {oneTest} from "./tests/oneTest.js";
+import {twoTests} from "./tests/twoTest.js";
+import {threeTest} from "./tests/threeTest.js";
+import {fourTest} from "./tests/fourTest.js";
+import {fiveTest} from "./tests/fiveTest.js";
+import {sixTest} from "./tests/sixTest.js";
+
+oneTest();
+twoTests();
+threeTest();
+fourTest();
+fiveTest();
+sixTest();
