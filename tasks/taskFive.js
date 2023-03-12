a499429214afac526c5ac0da2013ab5720d198e7
FryMondo <ptachyksergei@gmail.com>
2023-03-12 22:02:30 +0200

    task five v.1.1

--- tasks/taskFive.js
+++ tasks/taskFive.js
@@ -1,7 +1,8 @@
 import {student} from "../tests/fiveTest.js";
 
 function generateUsername(name, surname) {
-    if (!name || name.trim().length === 0 || !surname || surname.trim().length === 0) {
+    if (typeof name === "number" || typeof surname === "number" || !name || name.trim().length === 0 || !surname
+        || surname.trim().length === 0) {
         return undefined;
     } else {
         return `${name.toLowerCase()}.${surname.toLowerCase()}`;
@@ -9,18 +10,13 @@ function generateUsername(name, surname) {
 }
 
 function calculateAverageGrade(grades) {
-    if (grades.length === 0) {
-        return 0;
-    }
-    let sum = 0;
-    for (let mark of grades) {
-        if (!mark) {
-            sum += 0;
-        } else {
-            sum += (+mark);
-        }
-    }
-    return sum / grades.length;
+    const sum = grades.reduce((total, grade) => total + (+grade || 0));
+    return grades.length ? sum / grades.length : 0;
+}
+
+function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
 }
 
 const studentsMap = new Map();
@@ -28,7 +24,17 @@ const studentsMap = new Map();
 export const taskFive = {
     createStudent({firstName, lastName, security, faculty, course, ...grades}) {
         const username = generateUsername(firstName, lastName);
+        if (course !== null && course !== undefined && !isNaN(+course) && course >= 1 && course <= 5) {
+        } else {
+            throw new Error("Invalid course!");
+        }
+        if (faculty === null || faculty === undefined || typeof faculty !== 'string') {
+            throw new Error("Invalid faculty!");
+        }
         const {email} = security;
+        if (!isValidEmail(email)) {
+            throw new Error("Invalid email!");
+        }
         const validGrades = {};
         for (const [subject, grade] of Object.entries(grades)) {
             if (grade !== null && grade !== undefined && !isNaN(+grade)) {
