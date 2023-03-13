556fb4d30e7b202c294d984946be46c5fcd15a29
FryMondo <ptachyksergei@gmail.com>
2023-03-13 03:20:15 +0200

    test for task six v.1.1

--- tests/sixTest.js
+++ tests/sixTest.js
@@ -1,27 +1,79 @@
 import {assert} from "../asserts/assert.js";
-import {assertForObjects} from "../asserts/assertForObject.js";
 import {cityTemperaturesObject} from "../tasks/taskSix.js";
+import {assertForObjects} from "../asserts/assertForObject.js";
 
-//Num check
+export let temp;
+
+//Normal numbers
 function test1() {
-    assert(+cityTemperaturesObject, 11.25);
-}
-
-//String
-function test2() {
-    assert(`${cityTemperaturesObject}`, 'Kyiv: 12;\n' +
-        'Lviv: 10;\n' +
-        'Odesa: 15;\n' +
-        'Kharkiv: 8.');
-}
-
-//none
-function test3() {
-    assertForObjects(cityTemperaturesObject, {
+    temp = {
         Kyiv: 12,
-        Lviv: 10,
-        Odesa: 15,
-        Kharkiv: 8,
+        Lviv: '13',
+        Odesa: 9,
+        Kharkiv: 13,
+        Zhytomyr: 12,
+        Rivne: 11
+    };
+    assert(+cityTemperaturesObject, 11.67);
+}
+
+//"Wrong" numbers
+function test2() {
+    temp = {
+        Lutsk: null,
+        Cherkasy: 13,
+        Mariupol: 'ten',
+        Mykolaiv: 8,
+        Poltava: 10
+    };
+    assert(+cityTemperaturesObject, 0);
+}
+
+//Normal string and normal numbers
+function test3() {
+    temp = {
+        Kyiv: 12,
+        Lviv: '13',
+        Poltava: 10,
+        Cherkasy: 13,
+        Rivne: 11
+    };
+    assert(`${cityTemperaturesObject}`, 'Kyiv: 12;\n' +
+        'Lviv: 13;\n' +
+        'Poltava: 10;\n' +
+        'Cherkasy: 13;\n' +
+        'Rivne: 11.');
+}
+
+//Normal string and "wrong" numbers
+function test4() {
+    temp = {
+        Lutsk: null,
+        Odesa: 9,
+        Mariupol: 'ten',
+        Mykolaiv: 8,
+        Zhytomyr: 12
+    };
+    assert(`${cityTemperaturesObject}`, 'Invalid city!');
+}
+
+//"Wrong" string and normal numbers
+function test5() {
+    temp = {
+        Kyiv: 12,
+        Lviv: '13',
+        '1': 9,
+        Zhytomyr: 12,
+        undefined: 11
+    };
+    assert(`${cityTemperaturesObject}`, 'Invalid city!');
+}
+
+//Using nothing
+function test6() {
+    console.log(cityTemperaturesObject)
+    assertForObjects(cityTemperaturesObject, {
+        object: {'1': 9, Kyiv: 12, Lviv: '13', Zhytomyr: 12, undefined: 11}
     });
 }
 
@@ -45,4 +97,24 @@ export function sixTest() {
     } catch (error) {
         console.log('Something wrong in test #3');
     }
+    try {
+        test4();
+        console.log('Test #4 has passed successfully');
+    } catch (error) {
+        console.log('Something wrong in test #4');
+    }
+    try {
+        test5();
+        console.log('Test #5 has passed successfully');
+    } catch (error) {
+        console.log('Something wrong in test #5');
+    }
+    try {
+        test6();
+        console.log('Test #6 has passed successfully');
+    } catch (error) {
+        console.log('Something wrong in test #6');
+    }
 }
+
+sixTest()
